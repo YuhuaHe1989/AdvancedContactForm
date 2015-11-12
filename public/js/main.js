2928729aa94faf7e64e6b63a0440f6eb80a5f52e
yuhua he <yuhuahe@buffalo.edu>
2015-11-12 09:47:34 -0800

    update the data immediately

--- public/js/main.js
+++ public/js/main.js
@@ -15,22 +15,27 @@ function modalUpdate(){
   var $modalName = $('#modalInputName').val();
   var $modalEmail = $('#modalInputEmail').val();
   var $modalPhone = $('#modalInputPhone').val();
-  var $modalAdress = $('#modalInputAdress').val();
+  var $modalAddress = $('#modalInputAddress').val();
 
   $.get('/edit',function(data){
 
     if($modalName !== ''){
     data[editIndex].name = $modalName;
+    $('#sample:nth-child('+(editIndex + 1)+')').find('.name').text($modalName);
     }
     if($modalEmail !== ''){
     data[editIndex].email = $modalEmail;
+    $('#sample:nth-child('+(editIndex + 1)+')').find('.email').text($modalEmail);
     }
     if($modalPhone !== ''){
     data[editIndex].phone = $modalPhone;
+    $('#sample:nth-child('+(editIndex + 1)+')').find('.phone').text($modalPhone);
     }
-    if($modalAdress !== ''){
-    data[editIndex].address = $modalAdress;
+    if($modalAddress !== ''){
+    data[editIndex].address = $modalAddress;
+    $('#sample:nth-child('+(editIndex + 1)+')').find('.address').text($modalAddress);
     }
+
 
     $.post('/edit',{edit:data})
      .done(function(data){
@@ -39,7 +44,6 @@ function modalUpdate(){
      .fail(function(err){
        console.log(err);
       })
-
   })
 
   $('#myModal').modal('hide');
@@ -48,16 +52,20 @@ function modalUpdate(){
 
 function editContact(){
   editIndex = $(this).closest('tr').index();
+
 }
 
 function removeContact(){
   var index = $(this).closest('tr').index();
+  var deleteIndex = index + 1;
+
   $.get('/delete',function(data){
     data.splice(index,1);
     var del = JSON.stringify(data);
     
       $.post('/delete',{target:del})
        .done(function(data){
+        $('#sample:nth-child('+deleteIndex+')').remove();
         console.log(data);
         })
        .fail(function(err){
